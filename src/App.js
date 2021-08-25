5e6bfbfa839093f3cdc7b62915375a91a13ee9d1
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-25 11:58:13 +0200

    added a sidebar component and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Youtube Clone!</h1>
       <Header />
+
+      <div className="app__page">
+        <Sidebar />
+      </div>
 
     </div>
   );
