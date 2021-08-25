e71727e71cb31cbb062c1baec0b8d2d7c681cbc6
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-25 13:51:45 +0200

    updated recommended videos and styled

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -4,7 +4,10 @@ import '../css/RecommendedVideos.css';
 function RecommendedVideos() {
     return (
         <div className="recommendedVideos">
-            <h1>This is where videos go</h1>
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard />
+            </div>
         </div>
     )
 }
