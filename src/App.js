d3020598043eb9876aaa811a572919df9eea52c7
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-25 09:00:45 +0200

    created first header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Hello Youtube Clone!</h1>
+      <Header />
     </div>
   );
 }
