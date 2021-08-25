408154d95c9d6901c60144cd5e5501fc47400fb5
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-25 12:50:59 +0200

    sidebar component, icons and styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import RecommendedVideos from './components/RecommendedVideos';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <div className="app__page">
         <Sidebar />
+        <RecommendedVideos />
       </div>
 
     </div>
